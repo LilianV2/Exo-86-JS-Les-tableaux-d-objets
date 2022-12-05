2c54157296c08f4140a451108c346dbfc4faa1c8
lilia <lilianom59@outlook.fr>
2022-12-05 15:29:06 +0100

    obj2

--- app.js
+++ app.js
@@ -31,10 +31,16 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, peugeot, ford], // Vos voitures dans cette propriété !
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
 
-
+for (let i = 0; i < voitures.collection.length; i++){
+    voitures.collection[i].ouvrirPorte();
+    let div = document.getElementById('div')
+    let txt = document.createElement('p')
+    txt.innerHTML = voitures.collection[i].color;
+    div.appendChild(txt)
+}
